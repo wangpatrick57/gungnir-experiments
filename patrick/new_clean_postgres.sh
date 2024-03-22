c643b19d06e7044d9a53eca3bae14fe535779b2e
Patrick Wang <wang.patrick57@gmail.com>
2024-03-22 17:29:31 -0400

    good stuff

--- patrick/new_clean_postgres.sh
+++ patrick/new_clean_postgres.sh
@@ -4,11 +4,15 @@ rm -rf ~/pgdata
 cd ~/pgdata
 initdb
 cd -
-pg_ctl start
+pg_ctl start -D ~/pgdata
 
-# other
+# create users
 # default_user is used for cargo run --bin optd-perftest
 psql -d postgres -c "CREATE USER default_user WITH SUPERUSER PASSWORD 'password';"
 # test_user is used for cargo test --package optd-perftest
 psql -d postgres -c "CREATE USER test_user WITH SUPERUSER PASSWORD 'password';"
+
+# apply pgtune
 psql -d postgres -f patrick/pgtune.sql
+# MAKE SURE to restart postgres after applying pgtune
+pg_ctl restart -D ~/pgdata
